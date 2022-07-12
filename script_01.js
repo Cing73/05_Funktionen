9479c86c2532816033daa93882e7202a934fded3
Cing73 <craig@vancleaf>
2022-07-12 10:36:46 +0200

    return

--- script_01.js
+++ script_01.js
@@ -8,29 +8,78 @@
 // test();
 
 // Funktionsrumpf (body) | callee
-function test()
-{
-    console.log("Hallo Robert!");
-}
+// function test()
+// {
+//     console.log("Hallo Robert!");
+// }
 
-/***** Funktionen 02a *****/
+// ***** Funktionen 02a *****/
 // 2a. Parametrisierung + Datenübergabe von INNEN
 
 // ausgabeNamen()
 
-function ausgabeNamen() {
-    let firstName = "Anna";
-    console.log("Hallo " + firstName + "!");
-}
+// function ausgabeNamen() {
+//     let firstName = "Anna";
+//     console.log("Hallo " + firstName + "!");
+// }
 
 /***** Funktionen 02a *****/
 // 2a. Parametrisierung + Datenübergabe von INNEN
 
-ausgabeNamenParam("Anna"); // Argument
-ausgabeNamenParam("Craig");
-ausgabeNamenParam("Fatih");
+// ausgabeNamenParam("Anna"); // Argument
+// ausgabeNamenParam("Craig");
+// ausgabeNamenParam("Fatih");
 
-function ausgabeNamenParam(firstName) { // Parameter
+// function ausgabeNamenParam(firstName) { // Parameter
     
-    console.log("Hallo " + firstName + "!");
+//     console.log("Hallo " + firstName + "!");
+// }
+
+/***** Funktionen 02c *****/
+// 2c. Mehrere Parameter
+
+// ausgabeNamenParams("Maxine","Mütze");
+// ausgabeNamenParams(prompt("Vorname?"), prompt("Nachname?"));
+
+function ausgabeNamenParams(firstName, familyName) { // Parameter
+    console.log("Hallo " + firstName + " " + familyName + "!");
+    
+}
+
+/***** Funktionen 03a *****/
+// 03a. Vorbereitung
+// Postulat: one function = one job (Uncle Bob)
+// SRP Single Responsibility Principle
+
+// ausgabeNamenParams2("Max","Mütze");
+
+function ausgabeNamenParams2(firstName, familyName) { // Parameter
+
+    // 1. Funtionalität: string composing
+    const gap = " ";
+    const outputStr = "Hallo"+ gap + firstName + gap + familyName + "!";
+    
+ 
+    // 2. Funtionalität: data ouput
+    console.log(outputStr);
+}
+
+/***** Funktionen 03b *****/
+// Trennen der Funktionalitäten | return
+
+
+
+// 1. Funtionalität: string composing
+output(getString("Maxine","Mütze"));
+function getString(firstName, familyName) {
+    const gap = " ";
+    const outputStr = "Hallo"+ gap + firstName + gap + familyName + "!";
+    return outputStr; // ---> Daten zum Call
+    console.log("hi"); // return BEENDET die Funktion!
+}
+
+// 2. Funtionalität: data ouput
+// output("Hi");
+function output(outputData) {
+    console.log(outputData);
 }
